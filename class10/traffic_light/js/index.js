5c54f3e07e2646ed7861aff58f2c6c2f8b7b5a69
Amanda Rae <amanda.gaines@gmail.com>
2015-10-22 10:07:11 -0400

    created and modified in class work files
    Created and played with assignments for in-class work.

---
+++ class10/traffic_light/js/index.js
@@ -0,0 +1,30 @@
+//Implement the red light using jQuery. Don't forget to add the script tags.
+$(document).ready(function() {
+
+	function clearLights() {
+		$('.bulb').css('backgroundColor', 'black');
+		//query the doc for the class of bulb, then change the css of ('property', 'value')
+	}
+
+	function illuminateStopLightRed() {
+		clearLights();
+		$('#stopLight').css('backgroundColor', 'red');
+	}
+
+	function illuminateSlowLightYellow() {
+		clearLights();
+		$('#slowLight').css('backgroundColor', 'yellow');
+	}
+
+	function illuminateGoLightGreen() {
+		clearLights();
+		$('#goLight').css('backgroundColor', 'green');
+	}
+
+	$('#stopButton').click(illuminateStopLightRed);
+	//hquery the document for the ID stopButton, listen for a click, and follow the task of this variable
+
+	$('#slowButton').click(illuminateSlowLightYellow);
+	$('#goButton').click(illuminateGoLightGreen);
+
+});
