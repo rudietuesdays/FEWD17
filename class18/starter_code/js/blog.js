c272e508e06cca86a482dacfedc0124e77730338
Amanda Rae <amanda.gaines@gmail.com>
2015-12-01 21:41:36 -0500

    week 9 hw
    added files for hw, week 9

---
+++ class18/starter_code/js/blog.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+console.log('working!')
+
+})
+
+/********** Qs ********
+
+- bootstrap - how to connect to .js style
+- grids?!
+- js 'this'
+- positioning vs floats
+- when to use submit vs file vs button
+- when to use label vs legend
+- one js file for all pages? one per page?
+- egg drop?
+- help to look @ solution codes?
+- how long will have access to slides/repos etc?
+- designing for mobile - need to redo every element as @media?
+
+*/
